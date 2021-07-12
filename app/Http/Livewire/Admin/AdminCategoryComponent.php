ac17d6b739a4319e87c2d441b295a879f68e4ee9
Sharmila-prithula <sharmilarahmanprithula@gmail.com>
2021-07-13 01:22:12 +0600

    commit7

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -9,9 +9,15 @@ use Livewire\WithPagination;
 class AdminCategoryComponent extends Component
 {
     use WithPagination;
+    public function deleteCategory($id){
+        $category=Category::find($id);
+        $category->delete();
+        session()->flash('message','Category has been deleted successfully');
+    }
+
     public function render()
     {
-        $categories = Category::paginate(5);
+        $categories = Category::paginate(6);
         return view('livewire.admin.admin-category-component',['categories'=>$categories])->layout('layouts.base');
     }
 }
